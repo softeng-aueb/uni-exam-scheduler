426c1ed641e93d61f87a84d9c269c992a55bd84f
fanisth <fanis.th7@gmail.com>
2024-02-03 22:51:24 +0200

    - Supervisor Resource

---
+++ backend/src/main/java/gr/aueb/app/resource/AppUri.java
@@ -0,0 +1,7 @@
+package gr.aueb.app.resource;
+
+public class AppUri {
+
+    public static final String SUPERVISORS = "/supervisors";
+
+}
---
+++ backend/src/main/java/gr/aueb/app/resource/SupervisorResource.java
@@ -0,0 +1,89 @@
+package gr.aueb.app.resource;
+
+import gr.aueb.app.application.SupervisorService;
+import gr.aueb.app.domain.Supervisor;
+import gr.aueb.app.representation.SupervisorMapper;
+import gr.aueb.app.representation.SupervisorRepresentation;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+import java.net.URI;
+import java.util.List;
+
+import static gr.aueb.app.resource.AppUri.SUPERVISORS;
+
+@Path(SUPERVISORS)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RequestScoped
+public class SupervisorResource {
+
+    @Inject
+    SupervisorService supervisorService;
+
+    @Inject
+    SupervisorMapper supervisorMapper;
+
+
+    @Context
+    UriInfo uriInfo;
+
+    @GET
+    @Transactional
+    public List<SupervisorRepresentation> finnAll() {
+        return supervisorMapper.toRepresentationList(supervisorService.findAll());
+    }
+
+    @GET
+    @Path("/{supervisorId}")
+    @Transactional
+    public SupervisorRepresentation find(@PathParam("supervisorId") Integer supervisorId) {
+        try {
+            Supervisor foundSupervisor = supervisorService.findOne(supervisorId);
+            if(foundSupervisor == null) {
+                throw new NotFoundException();
+            }
+            return supervisorMapper.toRepresentation(foundSupervisor);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    @POST
+    @Transactional
+    public Response create (SupervisorRepresentation representation) {
+        // TODO check if toModel makes the validation we want
+        try {
+            Supervisor createdSupervisor = supervisorService.create(representation);
+            URI uri = UriBuilder.fromResource(SupervisorResource.class).path(String.valueOf(createdSupervisor.getId())).build();
+            return Response.created(uri).entity(supervisorMapper.toRepresentation(createdSupervisor)).build();
+        } catch(Exception e) {
+            throw new BadRequestException();
+        }
+    }
+
+//    @PUT
+//    @Path("/{id}")
+//    @Transactional
+//    public Response update(@PathParam("id") Integer id,
+//                           UserRepresentation representation) {
+//        User u = userRepository.findById(id);
+//        if(u == null){
+//            throw new NotFoundException();
+//        }
+//        // representation.id = id;
+//
+//
+//        User user = userMapper.toModel(representation);
+//
+//        user.setId(u.getId());
+//        userRepository.getEntityManager().merge(user);
+//
+//        return Response.noContent().build();
+//    }
+}
